2e4c718f41355afcf646a9672748daf18826203c
Arsen <rarsen2005@mail.ru>
2021-12-17 11:34:14 +0300

    comments and rebuilding of control

--- main_fail.py
+++ main_fail.py
@@ -24,6 +24,9 @@ def de_size(landmark, no_of_point, shape):
 
 # открытие детектора и все настройки
 
+color = (0, 255, 0)            # настроить цвет линии
+width_px = 7                   # настроить ширину линии
+
 handsDetector = mp.solutions.hands.Hands()
 cap = cv2.VideoCapture(0)
 drawings = [[]]
@@ -40,6 +43,8 @@ while cap.isOpened():
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
 
+    # крнвертирование для обработки изображения
+
     flipped = np.fliplr(frame)
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     results = handsDetector.process(flippedRGB)
@@ -47,6 +52,8 @@ while cap.isOpened():
     if results.multi_hand_landmarks is not None:
 
         right = 0
+
+        # вычислить, прямая ладонь или нет
 
         for finger in fingers:
             point_1 = finger[0]
@@ -59,22 +66,46 @@ while cap.isOpened():
             if 75 <= angle <= 105:
                 right += 1
 
+        # если ладонь прямая, то очистить экран
+
         if right == 4:
-            drawings = [[]]
+            break
 
-        (x_, y_), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
-        ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+        # распознание двух пальцев
 
-        if 2 * r / ws > 1.4:
+        x1_IF, y1_IF = de_size(results.multi_hand_landmarks[0].landmark, 8, flippedRGB.shape)
+        x2_IF, y2_IF = de_size(results.multi_hand_landmarks[0].landmark, 5, flippedRGB.shape)
+
+        index_finger = math.hypot(x1_IF - x2_IF, y1_IF - y2_IF)
+
+        x_MF, y_MF = de_size(results.multi_hand_landmarks[0].landmark, 12, flippedRGB.shape)
+        distance = math.hypot(x_MF - x1_IF, y_MF - y1_IF)
+
+        two_fingers = distance > index_finger / 4
+
+        # если пальци не близко то добавляем новую точку
+
+        if two_fingers:
             x = int(results.multi_hand_landmarks[0].landmark[8].x * flippedRGB.shape[1])
             y = int(results.multi_hand_landmarks[0].landmark[8].y * flippedRGB.shape[0])
 
             drawings[-1].append((x, y))
             to_append_new = True
+
         else:
             if to_append_new:
                 drawings.append([])
                 to_append_new = False
+
+        # распознование кулака
+
+        (x_, y_), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
+        ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+
+        fist = 2 * r / ws > 1.4
+
+        if not fist:
+            drawings = [[]]
 
     else:
         if to_append_new:
@@ -91,7 +122,7 @@ while cap.isOpened():
                 x2 = figure[line - 1][0]
                 y2 = figure[line - 1][1]
 
-                cv2.line(flippedRGB, (x1, y1), (x2, y2), (250, 0, 0), 7)
+                cv2.line(flippedRGB, (x1, y1), (x2, y2), color, width_px)
 
     # конвертирование обратно и выавод изображения
 
